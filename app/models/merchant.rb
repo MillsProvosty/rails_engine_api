834a3aab7cba24cb81eb3a450c02c85a61e84153
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-06-26 19:28:25 -0600

    Adds total revenue method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,12 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
   validates_presence_of :name
+
+  def self.total_revenue(quantity)
+    Merchant.joins(items: [:invoice_items])
+    .select("merchants.id, SUM(invoice_items.unit_price*invoice_items.quantity) AS revenue")
+    .group("merchants.id")
+    .order("revenue DESC")
+    .limit(quantity)
+  end
 end
