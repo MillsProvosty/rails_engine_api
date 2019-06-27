659e1408b01117fa136b8955fecaf4557477c110
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-06-26 19:44:22 -0600

    Changes relationships between transactions and invoices

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,7 +4,8 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
 
   def self.total_revenue(quantity)
-    Merchant.joins(items: [:invoice_items])
+    Merchant.joins(items: [invoice_items: [invoice: [:transaction.]]])
+    .where("transaction.result = ?", "success")
     .select("merchants.id, SUM(invoice_items.unit_price*invoice_items.quantity) AS revenue")
     .group("merchants.id")
     .order("revenue DESC")
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
+  describe 'relationships' do
+    it {should belong_to :invoices}
+  end
+  
   describe 'validations' do
     it {should validate_presence_of :invoice_id}
     it {should validate_presence_of :credit_card_number}
