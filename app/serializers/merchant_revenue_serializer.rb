313447f640d5a194e0beb393d45143b43732d8fb
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-06-30 17:13:32 -0600

    Finishes favorite customer

--- app/serializers/merchant_revenue_serializer.rb
+++ app/serializers/merchant_revenue_serializer.rb
@@ -5,16 +5,4 @@ class MerchantRevenueSerializer
   attribute :revenue do |merch|
     merch.total_revenue.to_s
   end
-
 end
-
-
-
-
-# class MovieSerializer
-#   include FastJsonapi::ObjectSerializer
-#
-#   attribute :name do |object|
-#     "#{object.name} Part 2"
-#   end
-# end
--- spec/requests/api/v1/merchant_requests_spec.rb
+++ spec/requests/api/v1/merchant_requests_spec.rb
@@ -209,5 +209,45 @@ describe "Merchants API" do
       total = JSON.parse(response.body)
       expect(total["data"]["attributes"]["revenue"]).to eq("10")
     end
+
+    it "returns the customer who has conducted the most total number of successful transactions" do
+      merch4 = create(:merchant)
+
+      item1 = create(:item, merchant: merch4, unit_price: 4)
+      item2 = create(:item, merchant: merch4, unit_price: 4)
+      item3 = create(:item, merchant: merch4, unit_price: 4)
+      item4 = create(:item, merchant: merch4, unit_price: 4)
+      item5 = create(:item, merchant: merch4, unit_price: 4)
+
+      cust1 = create(:customer)
+      cust2 = create(:customer)
+      cust3 = create(:customer)
+
+      invoice1 = create(:invoice, customer: cust1, merchant: merch4)
+      invoice2 = create(:invoice, customer: cust2, merchant: merch4)
+      invoice3 = create(:invoice, customer: cust3, merchant: merch4)
+      invoice4 = create(:invoice, customer: cust3, merchant: merch4)
+      invoice5 = create(:invoice, customer: cust3, merchant: merch4)
+
+      inv_item1 = create(:invoice_item, item: item1, quantity: 1, invoice: invoice1)
+      inv_item2 = create(:invoice_item, item: item2, quantity: 2, invoice: invoice2)
+      inv_item3 = create(:invoice_item, item: item3, quantity: 3, invoice: invoice3)
+      inv_item4 = create(:invoice_item, item: item4, quantity: 4, invoice: invoice4)
+      inv_item5 = create(:invoice_item, item: item4, quantity: 5, invoice: invoice5)
+
+      tran1 = create(:transaction, invoice: invoice1)
+      tran2 = create(:transaction, invoice: invoice2)
+      tran3 = create(:transaction, invoice: invoice3)
+      tran4 = create(:transaction, invoice: invoice4)
+      tran5 = create(:transaction, invoice: invoice5)
+
+      get "/api/v1/merchants/#{merch4.id}/favorite_customer"
+
+      expect(response).to be_successful
+
+      customer = JSON.parse(response.body)
+
+      expect(customer["data"]["attributes"]["favorite_customer"]["id"]).to eq(cust3.id)
+    end
   end
 end
